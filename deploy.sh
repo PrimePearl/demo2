c16955f7aa4529a0e5cbf27fc84bcea42ceaab75
Pearl Irhue <irhuepearl@gmai.com>
2024-01-13 16:08:25 +0000

    dev updated

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 
 knowlegdge and the implementation of that knowledge is what differentiates one man from another
 with the right knowledge and its implementation, you can conquer the world
+
+discipline is key
